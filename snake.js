1c4013dde75e6b4b931558ef875d0ffcaa3c3850
Carly Pennock <carly.pennock@gmail.com>
2020-01-25 17:40:06 -0600

    ratio fixed + variables added

--- snake.js
+++ snake.js
@@ -1,133 +1,151 @@
 let canvas = document.getElementById("game");
 let context = canvas.getContext("2d");
 
-let grid = 16;
+const GRID = 16;
+const SNEK_START_X = 10 * GRID;
+const SNEK_START_Y = 10 * GRID;
+const SNEK_START_DX = GRID;
+const SNEK_START_DY = 0;
+const SNEK_START_BODY_SEGMENTS = 4;
+const KEY = {
+	LEFT: 37,
+	UP: 38,
+	RIGHT: 39,
+	DOWN: 40,
+	SPACE: 32
+};
 let count = 0;
 
-let snake = {
-  x: 160,
-  y: 160,
+let snek = {
+	x: SNEK_START_X,
+	y: SNEK_START_Y,
 
-  // snek velocity. moves (1) grid length x or y
-  dx: grid,
-  dy: 0,
+	// snek velocity. moves (1) grid length x or y
+	dx: SNEK_START_DX,
+	dy: SNEK_START_DY,
 
-  // keep track of all grids snek occupies
-  cells: [],
+	// keep track of all grids snek occupies
+	cells: [],
 
-  // length of the snek
-  maxCells: 4
+	// length of the snek
+	bodySegments: SNEK_START_BODY_SEGMENTS
 };
+
 let food = {
-  x: 320,
-  y: 320
+	x: 320,
+	y: 320
 };
+
+// reassign food
+function newFood() {
+	food.x = getRandomInt(0, 25) * GRID;
+	food.y = getRandomInt(0, 25) * GRID;
+}
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 // animation loop
 function loop() {
-  requestAnimationFrame(loop);
+	requestAnimationFrame(loop);
 
-  // slow framerate - 60 fps to 15 fps (60/15 = 4)
-  if (++count < 4) {
-    return;
-  }
+	// slow framerate - 60 fps to 15 fps (60/15 = 4)
+	if (++count < 4) {
+		return;
+	}
 
-  count = 0;
-  context.clearRect(0, 0, canvas.width, canvas.height);
+	count = 0;
+	context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // velocity of snek
-  snake.x += snake.dx;
-  snake.y += snake.dy;
+	// velocity of snek
+	snek.x += snek.dx;
+	snek.y += snek.dy;
 
-  // wrap the snek
-  if (snake.x < 0) {
-    snake.x += canvas.width - grid;
-  } else if (snake.x >= canvas.width) {
-    snake.x = 0;
-  }
+	// wrap the snek
+	if (snek.x < 0) {
+		snek.x += canvas.width - GRID;
+	} else if (snek.x >= canvas.width) {
+		snek.x = 0;
+	}
 
-  if (snake.y < 0) {
-    snake.y += canvas.height - grid;
-  } else if (snake.y >= canvas.height) {
-    snake.y = 0;
-  }
+	if (snek.y < 0) {
+		snek.y += canvas.height - GRID;
+	} else if (snek.y >= canvas.height) {
+		snek.y = 0;
+	}
 
-  // keeps track of where snake has been
+	// keeps track of where snek has been
+	snek.cells.unshift({ x: snek.x, y: snek.y });
 
-  snake.cells.unshift({ x: snake.x, y: snake.y });
+	if (snek.cells.length > snek.bodySegments) {
+		snek.cells.pop();
+	}
 
-  if (snake.cells.length > snake.maxCells) {
-    snake.cells.pop();
-  }
+	// draw food for snek
+	context.fillStyle = "red";
+	context.fillRect(food.x, food.y, GRID - 1, GRID - 1);
 
-  // draw food for snek
+	// draw snek
+	context.fillStyle = "white";
+	snek.cells.forEach(function (cell, index) {
+		context.fillRect(cell.x, cell.y, GRID - 1, GRID - 1);
 
-  context.fillStyle = "red";
-  context.fillRect(food.x, food.y, grid - 1, grid - 1);
+		// snek eats food
+		if (cell.x === food.x && cell.y === food.y) {
+			snek.bodySegments++;
+			console.log(":D");
 
-  // draw snek
+			// reassign food
+			newFood();
 
-  context.fillStyle = "green";
-  snake.cells.forEach(function(cell, index) {
-    context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+			//   // reassign food
+			//   food.x = getRandomInt(0, 25) * grid;
+			//   food.y = getRandomInt(0, 25) * grid;
+		}
 
-    // snek eats food
-    if (cell.x === food.x && cell.y === food.y) {
-      snake.maxCells++;
-      console.log(":D");
+		// check for collision
+		for (let i = index + 1; i < snek.cells.length; i++) {
+			// restart game when snek collides with self
+			if (cell.x === snek.cells[i].x && cell.y === snek.cells[i].y) {
+				snek.x = 160;
+				snek.y = 160;
+				snek.cells = [];
+				snek.bodySegments = 4;
+				snek.dx = GRID;
+				snek.dy = 0;
 
-      // reassign food
-      food.x = getRandomInt(0, 25) * grid;
-      food.y = getRandomInt(0, 25) * grid;
-    }
+				console.log(":(");
 
-    // check for collision
-    for (let i = index + 1; i < snake.cells.length; i++) {
-      // restart game when snek collides with self
-      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-        snake.x = 160;
-        snake.y = 160;
-        snake.cells = [];
-        snake.maxCells = 4;
-        snake.dx = grid;
-        snake.dy = 0;
-
-        console.log(":(");
-
-        // reassign food
-        food.x = getRandomInt(0, 25) * grid;
-        food.y = getRandomInt(0, 25) * grid;
-      }
-    }
-  });
+				// reassign food
+				newFood();
+			}
+		}
+	});
 }
 
 // controls
-document.addEventListener("keydown", function(e) {
-  // left
-  if (e.which === 37 && snake.dx === 0) {
-    snake.dx = -grid;
-    snake.dy = 0;
-  }
-  // up
-  else if (e.which === 38 && snake.dy === 0) {
-    snake.dy = -grid;
-    snake.dx = 0;
-  }
-  // right
-  else if (e.which === 39 && snake.dx === 0) {
-    snake.dx = grid;
-    snake.dy = 0;
-  }
-  // down
-  else if (e.which === 40 && snake.dy === 0) {
-    snake.dy = grid;
-    snake.dx = 0;
-  }
+document.addEventListener("keydown", function (e) {
+	// left
+	if (e.which === 37 && snek.dx === 0) {
+		snek.dx = -GRID;
+		snek.dy = 0;
+	}
+	// up
+	else if (e.which === 38 && snek.dy === 0) {
+		snek.dy = -GRID;
+		snek.dx = 0;
+	}
+	// right
+	else if (e.which === 39 && snek.dx === 0) {
+		snek.dx = GRID;
+		snek.dy = 0;
+	}
+	// down
+	else if (e.which === 40 && snek.dy === 0) {
+		snek.dy = GRID;
+		snek.dx = 0;
+	}
 });
 
 // start game
